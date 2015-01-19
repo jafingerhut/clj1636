35300f7138a423d6fbbf2c4294d54a8369fbaf38
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2015-01-18 21:36:39 -0800

    Add bash script to run Midje tests with Clojure version having CLJ-1636 patch

---
+++ bin/midje-1636fix-test.sh
@@ -0,0 +1,38 @@
+#! /bin/bash
+
+if [ $# -lt 1 ]
+then
+    1>&2 echo "usage: `basename $0` <num-repetitions>"
+    exit 1
+fi
+NTRIALS="$1"
+
+ROOT=`pwd`
+if [ -d clojure/.git ]
+then
+    echo "Skipping getting of Clojure from github, since it appears you already have it."
+else
+    git clone git@github.com:clojure/clojure.git
+fi
+cd clojure
+# Get the first version with the fix for CLJ-1636
+git checkout f437b853adeaffc5cad9bb1e01e2355357a492c9
+# Built it and install it locally.  It will be called
+# clojure-1.7.0-master-SNAPSHOT in your local Maven repo
+mvn install
+
+cd ${ROOT}
+if [ -d Midje/.git ]
+then
+    echo "Skipping getting of Midje from github, since it appears you already have it."
+else
+    git clone git@github.com:marick/Midje.git
+fi
+cd Midje
+git checkout e98cf87
+# Copy over a project.clj that is nearly the same, except it uses
+# Clojure 1.7.0-master-SNAPSHOT for the profile named 1.7 instead of
+# 1.7.0-alpha5
+/bin/cp ${ROOT}/bin/midje-17snapshot-project.clj project.clj
+
+${ROOT}/bin/repeat.sh ${NTRIALS} ${ROOT}/bin/midje-1trial.sh
---
+++ bin/midje-17snapshot-project.clj
@@ -0,0 +1,45 @@
+(defproject midje "1.7.0-SNAPSHOT"
+  :description "A TDD library for Clojure that supports top-down ('mockish') TDD, encourages readable tests, provides a smooth migration path from clojure.test, balances abstraction and concreteness, and strives for graciousness."
+  :url "https://github.com/marick/Midje"
+  :pedantic? :warn
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 ;; upgrading to 1.3.2 produces record-type output for ordered maps
+                 [ordered "1.2.0" :exclusions [org.clojure/clojure]]
+                 [org.clojure/math.combinatorics "0.0.8"]
+                 ;; Changing following to 0.5.6 makes a t_unify test fail.
+                 [org.clojure/core.unify "0.5.2" :exclusions [org.clojure/clojure]]
+                 [clj-time "0.8.0" :exclusions [org.clojure/clojure]]
+                 [colorize "0.1.1" :exclusions [org.clojure/clojure]]
+                 [org.clojure/tools.macro "0.1.5"]
+                 [dynapath "0.2.0"]
+                 [org.tcrawley/dynapath "0.2.3"]
+                 [swiss-arrows "1.0.0" :exclusions [org.clojure/clojure]]
+                 [org.clojure/tools.namespace "0.2.7"]
+                 [flare "0.2.8" :exclusions [org.clojure/clojure]]
+                 [slingshot "0.12.1"]
+                 [commons-codec/commons-codec "1.10"]]
+  :profiles {:dev {:dependencies [[slamhound "1.5.5"]
+                                  [jonase/kibit "0.0.8"]
+                                  [prismatic/plumbing "0.3.5"]
+                                  [jonase/eastwood "0.2.0" :exclusions [org.clojure/clojure]]]
+                   :plugins [[lein-midje "3.1.4-SNAPSHOT"]]}
+             :test-libs {:dependencies [[prismatic/plumbing "0.3.5"]]}
+             :1.4 [:test-libs {:dependencies [[org.clojure/clojure "1.4.0"]]}]
+             :1.5.0 [:test-libs {:dependencies [[org.clojure/clojure "1.5.0"]]}]
+             :1.5.1 [:test-libs {:dependencies [[org.clojure/clojure "1.5.1"]]}]
+             :1.6 [:test-libs {:dependencies [[org.clojure/clojure "1.6.0"]]}]
+             :1.7 [:test-libs {:dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]]}]
+             ;; The following profile can be used to check that `lein with-profile`
+             ;; profiles are obeyed. Note that profile `:test-paths` *add on* to the
+             ;; defaults.
+             :test-test-paths {:test-paths ["test-test-paths"]}}
+  :resource-paths ["test-resources"]
+  :license {:name "The MIT License (MIT)"
+            :url "http://opensource.org/licenses/mit-license.php"
+            :distribution :repo}
+  :mailing-list {:name "Midje"
+                 :subscribe "https://groups.google.com/forum/?fromgroups#!forum/midje"}
+
+  ;; For Clojure snapshots
+  :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"
+                 "stuartsierra-releases" "http://stuartsierra.com/maven2"})
